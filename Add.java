641e5ae1f1ce6380882909c5d406b339ed2c458d
edarar <venkatarkr99@gmail.com>
2024-01-03 19:01:25 +0000

    java file Add.java modified

--- Add.java
+++ Add.java
@@ -1,12 +1,14 @@
 class Main {
 
   public static void main(String[] args) {
-    
+    //Initialise the variables
     int first = 10;
     int second = 20;
 
     // add two numbers
     int sum = first + second;
     System.out.println(first + " + " + second + " = "  + sum);
+	System.out.println(sum);
+
   }
 }
